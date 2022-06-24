feaa81c85fe9b6e6d39f382ee20588e98fc7f568
zhuyucun <1203407743@qq.com>
2022-06-24 11:38:00 +0800

    js方式暂存

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,41 +1,24 @@
+
 module.exports = {
-  title: 'zxxxx',
+  title: 'zyc',
   description: '学习记录，欢迎指正',
   base: '/',
   themeConfig: {
     siteTitle: 'siteTitle',
     nav: [
       {text: '首页', link: '/'},
-      {text: '文章', link: '/article/before'},
+      {text: '文章', link: '/article/index'},
       {
         text: '练习', 
         items: [
-          {text: 'HTML', link: '/HTML/'},
+          {text: 'HTML', link: '/HTML/index'},
           {text: 'CSS', link: '/CSS/index'},
           {text: 'JS', link: '/JS/index'},
         ]
       },
     ],
     sidebar: {
-      '/HTML/':[{
-        text: 'HTML1', 
-        collapsible: true,
-        items: [
-          { text: 'Introduction1', link: '/introduction' },
-          { text: 'Getting Started', link: '/getting-started' },
-        ]
-      }
-      ],
-      '/CSS/':[
-        { text: 'Introduction2', link: '/introduction' },
-        { text: 'Getting Started', link: '/getting-started' },
-      ],
-      '/JS/':[
-        { text: 'Introduction3', link: '/introduction' },
-        { text: 'Getting Started', link: '/getting-started' },
-      ],
       '/article/': []
-
     }
   },
   markdown: {
